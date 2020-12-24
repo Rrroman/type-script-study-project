bfbddc7f744c8c0d4bfdbfa023530330956ee91f
Roman Kohutyak <romankohutyak@gmail.com>
2020-12-25 01:24:52 +0200

    feat: learn about types syntax in ts

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Coding...');
+function add(adder, added, showResult, phrase) {
+    var result = adder + added;
+    if (showResult) {
+        console.log(resultPhrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 6;
+var number2 = 3.4;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
