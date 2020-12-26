c1e3c92578b5bcd62c88b92af1557a924ccc1f27
Roman Kohutyak <romankohutyak@gmail.com>
2020-12-26 22:22:03 +0200

    feat: basic class and this case in new object nick

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+class JobClass {
+  job: string;
+
+  constructor(jobParam: string) {
+    this.job = jobParam;
+  }
+
+  greet(this: JobClass) {
+    console.log(`Hello ${this.job}`);
+  }
+}
+
+const bob = new JobClass('director');
+bob.greet();
+
+const nick = { job: 'Waiter', greet: bob.greet };
+nick.greet();
