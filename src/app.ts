fea4aebaa545cffbf99dfecfb636de1d1481f0db
Roman Kohutyak <romankohutyak@gmail.com>
2020-12-26 22:59:34 +0200

    feat: privat field in class

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class JobClass {
   job: string;
+  private nameList: string[] = [];
 
   constructor(jobParam: string) {
     this.job = jobParam;
@@ -8,10 +9,29 @@ class JobClass {
   greet(this: JobClass) {
     console.log(`Hello ${this.job}`);
   }
+
+  hireWorker(name: string) {
+    this.nameList.push(name);
+  }
+
+  printWorkersListInfo() {
+    console.log(
+      `We hired ${this.nameList.length} workers. They are ${this.nameList}`,
+      this.nameList,
+    );
+  }
 }
 
-const bob = new JobClass('director');
+const bob = new JobClass('Director');
 bob.greet();
 
-const nick = { job: 'Waiter', greet: bob.greet };
-nick.greet();
+bob.hireWorker('Greg');
+bob.hireWorker('Gim');
+bob.hireWorker('Jonathan');
+
+// bob.nameList[6] = 'Nathan'; // Private field. Used to block this possibility to change array directly without method hireWorker().
+
+bob.printWorkersListInfo();
+
+// const jack = { job: 'Waiter', greet: bob.greet };
+// jack.greet();
