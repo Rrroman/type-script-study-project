133a148ff406dca2c41daae5b9913d87ad527bf6
Roman Kohutyak <romankohutyak@gmail.com>
2020-12-30 11:09:26 +0200

    feat: read only in interface

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 interface Greetable {
   name: string;
   age: number;
+  readonly dance: string;
 
   greet(phrase: string): void;
 }
@@ -8,6 +9,7 @@ interface Greetable {
 class Person implements Greetable {
   name: string;
   age: number;
+  dance = 'Polka';
   favoriteNumber = 21;
   constructor(name: string, age: number) {
     this.name = name;
@@ -16,11 +18,14 @@ class Person implements Greetable {
 
   greet(phrase: string) {
     console.log(
-      `${phrase} ${this.name}, my age is ${this.age}, favorite is ${this.favoriteNumber}`,
+      `${phrase} ${this.name}, my age is ${this.age}, favorite is ${this.favoriteNumber}, greeting you with ${this.dance} dance`,
     );
   }
 }
 
 const bob: Greetable = new Person('Bob', 42);
 bob.greet('Hello there, i am');
+// Read only property can't be changed because of interface Greetable.
+// Hint: Person class haven't dance as readonly
+// bob.dance = 'Break dance';
 console.log(bob);
