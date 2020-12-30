66a66e1412471bd0a2075e2e385d1dcadf3b8109
Roman Kohutyak <romankohutyak@gmail.com>
2020-12-30 16:54:49 +0200

    feat: intersection type

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log('it Works WOW');
+// interface Admin {
+type Admin = {
+  name: string;
+  hobbies: string[];
+};
+
+// interface Employee  {
+type Employee = {
+  name: string;
+  workDay: Date;
+};
+
+// interface ElevatedEmployee extends Admin , Employee {};
+type ElevatedEmployee = Admin & Employee;
+
+const worker: ElevatedEmployee = {
+  name: 'Richard',
+  hobbies: ['swimming, skating'],
+  workDay: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+// Hint type number must be used
+type Universal = Combinable & Numeric;
