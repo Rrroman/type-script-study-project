dc5aa632a4f641cc58b32ddb1c2d00f8952c7646
Roman Kohutyak <romankohutyak@gmail.com>
2020-12-31 14:13:20 +0200

    feat: generics

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('it Works WOW');
+const names: Array<string> = ['Bob'];
+
+console.log(names[0].split(''));
+
+const promise: Promise<number> = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(42);
+  }, 1000);
+});
+
+promise
+  .then((data): string => {
+    return data.toString();
+  })
+  .then((res) => console.log(res.split('')));
