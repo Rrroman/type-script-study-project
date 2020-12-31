ad6b7e010ebf46621770a33aac5612323f253183
Roman Kohutyak <romankohutyak@gmail.com>
2020-12-31 22:42:14 +0200

    feat: new generic function

--- src/app.ts
+++ src/app.ts
@@ -22,3 +22,21 @@ function merge<T extends object, U extends object>(obj1: T, obj2: U) {
 const merged = merge({ name: 'Roman', greet: 'Hello' }, { age: 31 });
 
 console.log(merged.name);
+
+interface Lengthy {
+  length: number;
+}
+
+function printLengthOfElements<T extends Lengthy>(element: T): [T, string] {
+  let result = 'No elements';
+  if (element.length === 1) {
+    result = `We got ${element.length} element`;
+  } else if (element.length > 1) {
+    result = `We have ${element.length} elements`;
+  }
+  return [element, result];
+}
+
+console.log(printLengthOfElements([]));
+console.log(printLengthOfElements(['Hi this is big string']));
+console.log(printLengthOfElements(['Lol', 'crab']));
