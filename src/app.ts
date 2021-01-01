88dc62af1fa49d0c784bd9015736583f49daa256
Roman Kohutyak <romankohutyak@gmail.com>
2021-01-01 15:40:19 +0200

    feat: decorators

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('it Works WOW');
+function Logger(constructor: Function) {
+  console.log('loading');
+  console.log(`Using: --> ${constructor}`);
+}
+@Logger
+class Human {
+  name = 'Roman';
+  constructor() {
+    console.log(
+      `Creating object with field -> ${Object.keys(this)} and value -> ${
+        this.name
+      }`,
+    );
+  }
+}
+
+const person = new Human();
